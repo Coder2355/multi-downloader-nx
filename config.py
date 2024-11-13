6f0f41badac95e4339c50805d46065706dbd6052
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-13 08:22:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ API_ID = int(os.environ.get("API_ID", "21740783"))
 API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
 
 # Crunchyroll credentials
-CR_USERNAME = "Doordasj@gmail.com"
-CR_PASSWORD = "MikiMiki51"
+CR_USERNAME = "zayedthaer@yahoo.com"
+CR_PASSWORD = "T-raw360"
