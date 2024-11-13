7f65fa404a639129b915ede3d824ec99dc1dbaf1
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-13 08:15:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+# config.py
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7444872585:AAHYzPX_gygFh9xYvu0-k7YOUg7BSG_hzHg")
+API_ID = int(os.environ.get("API_ID", "21740783"))
+API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+
+# Crunchyroll credentials
+CR_USERNAME = "Doordasj@gmail.com"
+CR_PASSWORD = "MikiMiki51"
